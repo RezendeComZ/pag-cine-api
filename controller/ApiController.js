e2f942c4aacee8d6bb0a9c4f01a0b9ea8c37abb4
Gabriel Rezende <g@brielrezen.de>
2021-03-23 16:13:19 -0300

    refs para banco de dados

--- controller/ApiController.js
+++ controller/ApiController.js
@@ -1,7 +1,15 @@
+const Sequelize = require('sequelize');
+const config = require('../config/database')
+
 const ApiController = {
   index: function(req, res, next) {
-  res.render('index', { title: 'Express' });
-}
+    const db = new Sequelize('config');
+    db.query('SELECT * FROM filme');
+    res.render('index', { title: 'Express' });
+  },
+  filmes: (req, res) => {
+    res.render('filmes')
+  }
 }
 
 module.exports = ApiController
