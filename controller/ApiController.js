5e518a4fef7d63087fe8fa4dbf2e395caacb093b
Gabriel Rezende <g@brielrezen.de>
2021-03-23 19:10:48 -0300

    recebendo informações do banco

--- controller/ApiController.js
+++ controller/ApiController.js
@@ -2,12 +2,13 @@ const Sequelize = require('sequelize');
 const config = require('../config/database')
 
 const ApiController = {
-  index: function(req, res, next) {
-    const db = new Sequelize('config');
-    db.query('SELECT * FROM filme');
+  index: async (req, res, next) => {
     res.render('index', { title: 'Express' });
   },
-  filmes: (req, res) => {
+  filmes: async (req, res) => {
+    const db = new Sequelize(config);
+    const filmes = await db.query('select * from filme;', {type: Sequelize.QueryTypes.SELECT});
+    console.log(filmes)
     res.render('filmes')
   }
 }
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', ApiController.index);
+router.get('/filmes', ApiController.filmes);
 
 module.exports = router;
