db591b6d37fcfdcf64ff1f0ce3117c6f638b1621
Gabriel Rezende <g@brielrezen.de>
2021-03-24 08:03:12 -0300

    nova rota

--- controller/ApiController.js
+++ controller/ApiController.js
@@ -1,15 +1,20 @@
 const Sequelize = require('sequelize');
 const config = require('../config/database')
+const { Filme } =  require('../models')
 
 const ApiController = {
   index: async (req, res, next) => {
     res.render('index', { title: 'Express' });
   },
-  filmes: async (req, res) => {
+  filmesQuery: async (req, res) => { // usando somente queries
     const db = new Sequelize(config);
     const filmes = await db.query('select * from filme;', {type: Sequelize.QueryTypes.SELECT});
     console.log(filmes)
     res.render('filmes')
+  },
+  filmes: async (req, res) => {
+    let filmes = await Filme.findAll();
+    console.log(filmes)
   }
 }
 
